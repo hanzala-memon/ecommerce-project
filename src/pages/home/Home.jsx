029bd6ce7e2dbd339de57219494bb801608b98e2
Hanzala Yousuf <hanzalayousuf159@gmail.com>
2023-10-04 20:35:54 +0500

    First commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react'
+import Layout from '../../components/layout/Layout'
+import myContext from '../../context/data/myContext'
+import HeroSection from '../../components/herosection/HeroSection'
+import Filter from '../../components/filter/Filter'
+import ProductCard from '../../components/productCard/ProductCard'
+import Testimonial from '../../components/testimonial/Testimonial'
+import Track from '../../components/track/Track'
+
+function Home() {
+  
+  return (
+    <Layout>
+      <HeroSection />
+      <Filter />
+      <ProductCard />
+      <Track />
+      <Testimonial />
+      
+      
+    </Layout>
+  )
+}
+
+export default Home
